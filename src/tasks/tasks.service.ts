5d91d514040ed32551678032d7bfb730a7fcaf89
Dinis Esteves <dinisjesteves@gmail.com>
2022-05-11 23:44:44 +0100

    feat(tasks): create task model

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
 
 @Injectable()
 export class TasksService {
-    private tasks = [
-        "foo",
-        "bar"
-    ];
+    private tasks: Task[] = [];
 
     /*
     * Defining the acessor in this case might be optional
     * by default it is public.
     */
-    public get() {
+    public get(): Task[] {
         return this.tasks;
     }
 }
