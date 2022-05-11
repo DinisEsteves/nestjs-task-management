42abbeb922b04e0a27cf870bdc463dcfccb44bab
Dinis Esteves <dinisjesteves@gmail.com>
2022-05-11 23:38:47 +0100

    feat(tasks): get tasks method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks = [
+        "foo",
+        "bar"
+    ];
+
+    /*
+    * Defining the acessor in this case might be optional
+    * by default it is public.
+    */
+    public get() {
+        return this.tasks;
+    }
+}
