504d5189e9516752ca14e9d8ad9515bf152c0b63
Dinis Esteves <dinisjesteves@gmail.com>
2022-11-22 20:51:29 +0000

    feat(tasks):add optional validation search dto

--- src/tasks/dto/filter-task.dto.ts
+++ src/tasks/dto/filter-task.dto.ts
@@ -1,9 +1,12 @@
-import { IsEnum } from "class-validator";
+import { IsEnum, IsNotEmpty, IsOptional } from "class-validator";
 import { TaskStatus } from "../task.model";
 
 export class FilterTaskDto {
+    @IsOptional()
+    @IsNotEmpty()
     search?: string;
 
+    @IsOptional()
     @IsEnum(TaskStatus)
     status?: TaskStatus;
 }
